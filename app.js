fc65631d364a2e586fabdedd47d38524ca7794eb
skanderbac <skanderbac@users.noreply.github.com>
2022-06-10 10:01:09 +0100

    get new joiners

--- app.js
+++ app.js
@@ -98,6 +98,85 @@ app.get('/token', function (req, res) {
     });
 });
 
+app.get('/amani',(req,res)=>{
+    const response ={
+        "nom":'amani',
+        "prenom":"amani"
+    };
+    return res.status(200).send(response);
+});
+
+app.post('/amani',(req,res)=>{
+    const body = req.body;
+    const response = {
+        "result":`user created: ${body.name}`
+    }
+
+    return res.status(200).send(response);
+});
+
+app.get('/newjoiners',(req,res)=>{
+    const newjoiners = [
+        {
+            isFavorite: false,
+            name: 'Salha',
+            phone: '770-504-2217',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
+            email: 'salha@gmail.com',
+            interest: 'C++',
+        },
+        {
+            isFavorite: true,
+            name: 'Richard Mahoney',
+            phone: '423-676-2869',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/1.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        },
+        {
+            isFavorite: false,
+            name: 'Donna Price',
+            phone: '859-496-2817',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/50.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        },
+        {
+            isFavorite: false,
+            name: 'Lisa Landers',
+            phone: '901-747-3428',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/3.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        },
+        {
+            isFavorite: false,
+            name: 'Terrance Orta',
+            phone: '770-504-2217',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        },
+        {
+            isFavorite: false,
+            name: 'Terrance Orta',
+            phone: '770-504-2217',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/men/27.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        },
+        {
+            isFavorite: true,
+            name: 'Dorothy H. Spencer',
+            phone: '573-394-9254',
+            photo: 'https://www.infragistics.com/angular-demos-lob/assets/images/women/67.jpg',
+            email: 'ameni@gmail.com',
+            interest: 'java',
+        }
+    ];
+    return res.status(200).send(newjoiners);
+})
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
